1bd704d04b51173578c2ca855157a78d9ebe98a1
Vegadrone <spenaantonino@gmail.com>
2022-09-05 16:31:59 +0200

    Scaffolding and create class product

---
+++ classes/Product.php
@@ -0,0 +1,30 @@
+<?php 
+
+    class Product{
+        protected $brand;
+        protected $description;
+
+        public function __construct($_brand, $_description){
+            $this-> brand = $_brand;
+            $this-> description = $_description;
+        }
+
+        public function setBrand($_brand){
+            $this-> brand = $_brand; 
+        }
+
+        public function getBrand(){
+            return $this-> brand;
+        }
+
+        public function setDescription($_description){
+            $this-> description = $_description;
+        }
+
+        public function getDescription(){
+            return $this-> description;
+        }
+
+    }
+
+?>
